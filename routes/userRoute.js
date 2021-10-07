07c942b73fd930bdf90c7203b95fef38c9a96a0f
LegalPirates2020 <legalpirates2020@gmail.com>
2021-10-07 14:56:28 +0530

    Changes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,70 +1,62 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const mysql = require("mysql");
+const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "classicneetauth",
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'classicneetauth'
 });
 
-
-router.route("/home").get((req, res) => {
-  
-
-  db.query(
-    "SELECT * FROM homeslider",
-    (error, response) => {
-      var arr = []
-      if (error) {
-        // console.log(imganame);
-        console.log(error);
-      } else {
-        console.log(response.length);
-        for(let i = 0; i <= response.length-1 ; i++){
-          
-          var image = {sliderimg:response[i].sliderimg, imgname: response[i].imgname}
-          // console.log(image);
-          arr.push(image)
-        }
-        // console.log(response[0].sliderimg);
-        console.log(arr);
-        res.render("home",{img : arr});
-      }
-    }
-  );
-  
+router.route('/home').get((req, res) => {
+	db.query('SELECT * FROM homeslider', (error, response) => {
+		var arr = [];
+		if (error) {
+			// console.log(imganame);
+			console.log(error);
+		} else {
+			console.log(response.length);
+			for (let i = 0; i <= response.length - 1; i++) {
+				var image = {
+					sliderimg: response[i].sliderimg,
+					imgname: response[i].imgname
+				};
+				// console.log(image);
+				arr.push(image);
+			}
+			// console.log(response[0].sliderimg);
+			console.log(arr);
+			res.render('home', { img: arr });
+		}
+	});
 });
 
+router.route('/adminheader').get((req, res) => {
+	db.query('SELECT * FROM homeslider', (error, response) => {
+		var arr = [];
+		if (error) {
+			// console.log(imganame);
+			console.log(error);
+		} else {
+			console.log(response.length);
+			for (let i = 0; i <= response.length - 1; i++) {
+				var image = {
+					sliderimg: response[i].sliderimg,
+					imgname: response[i].imgname
+				};
+				// console.log(image);
+				arr.push(image);
+			}
+			// console.log(response[0].sliderimg);
+			console.log(arr);
+			res.render('admin', { img: arr });
+		}
+	});
+});
 
-router.route("/adminheader").get((req, res) => {
-  db.query("SELECT * FROM homeslider", (error, response) => {
-    var arr = [];
-    if (error) {
-      // console.log(imganame);
-      console.log(error);
-    } else {
-      console.log(response.length);
-      for (let i = 0; i <= response.length - 1; i++) {
-        var image = {
-          sliderimg: response[i].sliderimg,
-          imgname: response[i].imgname,
-        };
-        // console.log(image);
-        arr.push(image);
-      }
-      // console.log(response[0].sliderimg);
-      console.log(arr);
-      res.render("admin", { img: arr });
-    }
-  });
-
-
-
-  
+router.get('/contact', (req, res) => {
+	res.render('contact');
 });
 
 module.exports = router;
-
-
