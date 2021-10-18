87aa21545d356ecea997ba3ae481ad65211ff686
lohithsrk <srklohith05@gmail.com>
2021-10-18 16:44:51 +0530

    Chnages

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,47 +1,81 @@
-let count1 = 1;
-let count2 = 1;
-let count3 = 1;
-let count4 = 1;
+// let count1 = 1;
+// let count2 = 1;
+// let count3 = 1;
+// let count4 = 1;
 
-function MBBS_Seats() {
-	count1++;
-	document.querySelector('.MBBS_Seats').innerHTML = count1;
-	if (
-		count1 == document.querySelector('.MBBS_Seats').getAttribute('data-target')
-	) {
-		clearInterval(MBBS_Seats);
-	}
-}
+// function MBBS_Seats() {
+// 	count1++;
+// 	document.querySelector('.MBBS_Seats').innerHTML = count1;
+// 	if (
+// 		count1 == document.querySelector('.MBBS_Seats').getAttribute('data-target')
+// 	) {
+// 		clearInterval(MBBS_Seats);
+// 	}
+// }
 
-function Consecutive() {
-	count2++;
-	document.querySelector('.Consecutive').innerHTML = count2;
-	if (
-		count2 == document.querySelector('.Consecutive').getAttribute('data-target')
-	) {
-		clearInterval(Consecutive);
-	}
-}
+// function Consecutive() {
+// 	count2++;
+// 	document.querySelector('.Consecutive').innerHTML = count2;
+// 	if (
+// 		count2 == document.querySelector('.Consecutive').getAttribute('data-target')
+// 	) {
+// 		clearInterval(Consecutive);
+// 	}
+// }
 
-function successRate() {
-	count3++;
-	document.querySelector('.successRate').innerHTML = `${count3}%`;
-	if (
-		count3 == document.querySelector('.successRate').getAttribute('data-target')
-	) {
-		clearInterval(successRate);
-	}
-}
+// function successRate() {
+// 	count3++;
+// 	document.querySelector('.successRate').innerHTML = `${count3}%`;
+// 	if (
+// 		count3 == document.querySelector('.successRate').getAttribute('data-target')
+// 	) {
+// 		clearInterval(successRate);
+// 	}
+// }
 
-function admission() {
-	count4++;
-	document.querySelector('.admission').innerHTML = `${count4}%`;
-	if (
-		count4 == document.querySelector('.admission').getAttribute('data-target')
-	) {
-		clearInterval(admission);
-	}
-}
+// function admission() {
+// 	count4++;
+// 	document.querySelector('.admission').innerHTML = `${count4}%`;
+// 	if (
+// 		count4 == document.querySelector('.admission').getAttribute('data-target')
+// 	) {
+// 		clearInterval(admission);
+// 	}
+// }
+
+// number count for stats, using jQuery animate
+window.addEventListener('scroll',(e)=>{
+	console.log();
+})
+$('.counter').each(function () {
+	var $this = $(this),
+		countTo = $this.attr('data-count');
+	console.log(countTo);
+
+	$({ countNum: $this.text() }).animate(
+		{
+			countNum: countTo
+		},
+		{
+			duration: 1000,
+			easing: 'linear',
+			step: function () {
+				if ($this.attr('class').includes('successRate')) {
+					$this.text(`${Math.floor(this.countNum)}%`);
+				} else {
+					$this.text(Math.floor(this.countNum));
+				}
+			},
+			complete: function () {
+				if ($this.attr('class').includes('successRate')) {
+					$this.text(`${this.countNum}%`);
+				} else {
+					$this.text(`${this.countNum}`);
+				}
+			}
+		}
+	);
+});
 
 var slideIndex = 1;
 showSlides(slideIndex);
