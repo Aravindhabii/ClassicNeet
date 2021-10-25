374f2e822af215a3d98fcb254565578357284a69
solomonsanthosh <solosanty2064@gmail.com>
2021-10-25 13:12:29 +0530

    Ss

--- public/javascripts/courses.js
+++ public/javascripts/courses.js
@@ -42,15 +42,46 @@ const para1 = document.querySelector(".para1");
 
 $(window)
   .resize(function () {
+    if (window.innerWidth < 600) {
+        console.log('lk');
+      $(".para1neet").replaceWith(
+          
+        "<p class='hexContent'>This course is meant for those who have completed their 12th...</p>"
+      );
+      $(".para2neet").replaceWith(
+          
+        "<p class='hexContent'>This course is meant for students who have completed their 10th...</p>"
+      );
+
+      $(".para3neet").replaceWith(
+          
+        "<p class='hexContent'>The course is specifically designed for those who wish to focus on NEET...</p>"
+      );
+     
+      $(".para4neet").replaceWith(
+          
+        "<p class='hexContent'>This course is meant for those who are appearing for their 12th...</p>"
+      );
+    }
     if (window.innerWidth < 750) {
         console.log('jk');
-      $(".para1").replaceWith(
+      $(".para1neet").replaceWith(
         "<p class='hexContent'>This course is meant for those who have completed their 12th classboard exams and might have already attempted the NEET.</p>"
       );
+      $(".para2neet").replaceWith(
+          
+        "<p class='hexContent'>This course is meant for students who have completed their 10th std board exams.</p>"
+      );
+      $(".para4neet").replaceWith(
+          
+        "<p class='hexContent'>This course is meant for those who are appearing for their 12th class board exams and wish to focus on NEET after the Board exams.</p>"
+      );
     } else if (window.innerWidth > 750) {
-        $(".para1").replaceWith(
+        $(".para1neet").replaceWith(
             "<p class='hexContent'>This course is meant for those who have completed their 12th classboard exams and might have already attempted the NEET. They willappear for NEET in the coming year after one year of trainingfocused on NEET.</p>"
           );
     }
+   
+    
   })
   .resize();
