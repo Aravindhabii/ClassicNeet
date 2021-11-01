80b1a8d93e5672cd708f07766b2b2527a0c223f3
lohithsrk <srklohith05@gmail.com>
2021-11-01 15:25:21 +0530

    Changes

--- public/javascripts/chatbot.js
+++ public/javascripts/chatbot.js
@@ -4,3 +4,13 @@ const chat = document.querySelector('.chat');
 botImg.addEventListener('click', () => {
 	chat.classList.toggle('open');
 });
+
+window.addEventListener('scroll', (e) => {
+    var scroll = window.scrollY;
+    if(scroll>=100){
+        botImg.style.top = '100vh';
+    }else{
+        botImg.style.top = '120vh';
+        
+    }
+})
