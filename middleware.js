84f115714d254cf445fddf9e7409175d6d4a9e4d
lohithsrk <srklohith05@gmail.com>
2021-11-15 12:46:01 +0530

    Changes

--- middleware.js
+++ middleware.js
@@ -1,9 +1,9 @@
-module.exports.isloggedin = (req,res,next)=>{
-    const sess = req.session.loginuser
-    const yep = 'Yes'
-    if (sess !== yep) {
-        res.redirect('/login');
-    }else{
-        next();
-    }
-}
+module.exports.isloggedin = (req, res, next) => {
+	const sess = req.session.loginuser;
+	const yep = 'Yes';
+	if (sess !== yep) {
+		res.redirect('/login');
+	} else {
+		next();
+	}
+};
