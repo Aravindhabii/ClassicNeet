0701b6dbec03cd45e128eaa657da23a0d358443d
lohithsrk <srklohith05@gmail.com>
2021-11-16 12:52:51 +0530

    Chaneg

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-const express = require("express");
-const path = require("path");
-const ejsMate = require("ejs-mate");
-const session = require("express-session");
-const flash = require("connect-flash");
-var cookieParser = require("cookie-parser");
-const userRoutes = require("./routes/userRoute");
-const authentication = require("./routes/authentication");
-const db = require("./database");
-const methodOverride = require("method-override");
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
+const express = require('express');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+var cookieParser = require('cookie-parser');
+const userRoutes = require('./routes/userRoute');
+const authentication = require('./routes/authentication');
+const db = require('./database');
+const methodOverride = require('method-override');
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 db.connect((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Mysql connected");
-  }
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Mysql connected');
+	}
 });
 
 const app = express();
 
 const sessionConfig = {
-  secret: "thisshouldbeasecret!",
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    expires: Date.now() + 1000 * 60 * 60,
-    maxAge: 1000 * 60 * 60,
-  },
+	secret: 'thisshouldbeasecret!',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true,
+		expires: Date.now() + 1000 * 60 * 60,
+		maxAge: 1000 * 60 * 60
+	}
 };
 
 app.use(session(sessionConfig));
-app.use(methodOverride("_method"));
-app.engine("ejs", ejsMate);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(flash());
 
@@ -50,8 +50,6 @@ app.use(flash());
 // 	// Get an array of flash messages by passing the key to req.flash()
 // 	res.send('hello from / ', { });
 //   });
-
-
 
 // app.use( (req, res,next) => {
 // 	console.log("hello");
@@ -77,21 +75,32 @@ app.use(flash());
 // req.flash('error', 'You do not have permission to do that!');
 // req.flash('success', 'Successfully made a new campground!');
 
-app.use("/", userRoutes);
-app.use("/", authentication);
+app.use('/', userRoutes);
+app.use('/', authentication);
 
-app.get('/fun',(req,res)=>{
-	req.flash('success', 'you have been successfully loggedin')
-	res.render("404error")
-})
-app.get("/stories", (req, res) => {
-  res.render("successStories");
+app.get('/fun', (req, res) => {
+	req.flash('success', 'you have been successfully loggedin');
+	res.render('404error');
+});
+app.get('/stories', (req, res) => {
+	res.render('successStories');
 });
 
-app.get("*", (req, res) => {
-  res.render("404error");
+app.get('*', (req, res) => {
+	res.render('404error');
 });
 
 app.listen(process.env.PORT, () =>
-  console.log(`SERVER IS RUNNING ON PORT ${process.env.PORT}`)
+	console.log(`SERVER IS RUNNING ON PORT ${process.env.PORT}`)
 );
+module.exports = {
+	apps: [
+		{
+			script: 'app.js',
+			watch: ['server', 'client'],
+			// Delay between restart
+			watch_delay: 1000,
+			ignore_watch: ['node_modules', 'client/img']
+		}
+	]
+};
