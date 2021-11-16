b8e018c91bec47cf7bd31c0dc6a0b8be5f064355
lohithsrk <srklohith05@gmail.com>
2021-11-16 21:56:20 +0530

    Changes

--- public/javascripts/admin/results/studentdetails.js
+++ public/javascripts/admin/results/studentdetails.js
@@ -1,45 +1,44 @@
-const addbtn = document.querySelector(".addbtn");
-const previewDiv = document.querySelector(".preview");
-const backbtn = document.querySelector(".backbtn");
-const sectionop = document.querySelector(".mainsection");
-const imgform = document.querySelector(".imgform");
+const addbtn = document.querySelector('.addbtn');
+const previewDiv = document.querySelector('.preview');
+const backbtn = document.querySelector('.backbtn');
+const sectionop = document.querySelector('.mainsection');
+const imgform = document.querySelector('.imgform');
 const submitbtn = document.querySelector('.submitbtn');
 
-addbtn.addEventListener("click", () => {
-  previewDiv.style.display = "flex";
-  imgform.style.transform = "translate(300vw,0)";
-  previewDiv.style.transform = "none";
+addbtn.addEventListener('click', () => {
+	previewDiv.style.display = 'flex';
+	imgform.style.transform = 'translate(300vw,0)';
+	previewDiv.style.transform = 'none';
 });
 
-backbtn.addEventListener("click", () => {
-  previewDiv.style.transform = "translate(300vw,0)";
-  imgform.style.transform = "translate(0,0)";
+backbtn.addEventListener('click', () => {
+	previewDiv.style.transform = 'translate(300vw,0)';
+	imgform.style.transform = 'translate(0,0)';
 });
 
-const previewTopper = document.querySelector(".previewTopper");
-const previewSpan = document.querySelectorAll(".currentPreviewSpan");
-const closesvg = document.querySelector(".closesvg");
-const sliderimgurl = document.querySelectorAll(".currentSliderimgurl");
+const previewTopper = document.querySelector('.previewTopper');
+const previewSpan = document.querySelectorAll('.currentPreviewSpan');
+const closesvg = document.querySelector('.closesvg');
+const sliderimgurl = document.querySelectorAll('.currentSliderimgurl');
 for (let i = 0; i <= previewSpan.length - 1; i++) {
-  previewSpan[i].addEventListener("click", (e) => {
-    for (let j = 0; j <= sliderimgurl.length - 1; j++) {
-      if (i === j) {
-        const img = document.createElement("img");
-        if (sliderimgurl[j].innerText.length < 1) return;
-        img.src = sliderimgurl[j].innerText;
-        previewTopper.appendChild(img);
-        previewTopper.style.display = "flex";
-        sectionop.style.filter = "blur(20px)";
-        closesvg.addEventListener("click", () => {
-          previewTopper.removeChild(img);
-          previewTopper.style.display = "none";
-          sectionop.style.filter = "blur(0px)";
-        });
-      }
-    }
-  });
+	previewSpan[i].addEventListener('click', (e) => {
+		for (let j = 0; j <= sliderimgurl.length - 1; j++) {
+			if (i === j) {
+				const img = document.createElement('img');
+				if (sliderimgurl[j].innerText.length < 1) return;
+				img.src = sliderimgurl[j].innerText;
+				previewTopper.appendChild(img);
+				previewTopper.style.display = 'flex';
+				sectionop.style.filter = 'blur(20px)';
+				closesvg.addEventListener('click', () => {
+					previewTopper.removeChild(img);
+					previewTopper.style.display = 'none';
+					sectionop.style.filter = 'blur(0px)';
+				});
+			}
+		}
+	});
 }
-<<<<<<< HEAD
 
 submitbtn.addEventListener('click', () => {
 	Swal.fire({
@@ -56,14 +55,12 @@ submitbtn.addEventListener('click', () => {
 		}
 	});
 });
-=======
-document.querySelectorAll(".updatesCheckbox").forEach((check, i) => {
-  check.addEventListener("change", () => {
-    if (document.querySelectorAll('input[type="checkbox"]:checked').length) {
-      document.querySelector(".submit").removeAttribute("disabled");
-    } else {
-      document.querySelector(".submit").setAttribute("disabled", true);
-    }
-  });
+document.querySelectorAll('.updatesCheckbox').forEach((check, i) => {
+	check.addEventListener('change', () => {
+		if (document.querySelectorAll('input[type="checkbox"]:checked').length) {
+			document.querySelector('.submit').removeAttribute('disabled');
+		} else {
+			document.querySelector('.submit').setAttribute('disabled', true);
+		}
+	});
 });
->>>>>>> 76a13649524d14e50eb497daf085439ec40ec89d
