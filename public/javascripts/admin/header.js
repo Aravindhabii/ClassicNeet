c442f57c4a43e713092d2e3d86a53afb94b455b5
Aravindhabii <abiiaravindhr.it2020@citchennai.net>
2021-11-17 11:16:48 +0530

    changes

--- public/javascripts/admin/header.js
+++ public/javascripts/admin/header.js
@@ -1,9 +1,12 @@
-const collapse = document.querySelectorAll('.collapse');
+const collapse = document.querySelectorAll(".collapse");
+const collapsed = document.querySelectorAll(".collapsed");
 
 console.log(collapse);
 
-collapse.forEach(function(element) {
-  element.addEventListener('click', function() {
-    element.classList.remove('show');
+collapsed.forEach(function (element) {
+  collapse.forEach(function (element2) {
+    element.addEventListener("mouseover", (e) => {
+      e.classList.add("show");
+    });
   });
 });
