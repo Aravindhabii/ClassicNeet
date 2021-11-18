40aca1a60eff0b41354e7d45a1c8876d0748deee
Prasanna-13-RV <prasannavelmurugan0200@gmail.com>
2021-11-18 15:23:23 +0530

    changes

--- public/javascripts/nav.js
+++ public/javascripts/nav.js
@@ -1,17 +1,17 @@
-var mybutton = document.querySelector('.topButton');
+// var mybutton = document.querySelector('.topButton');
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-	scrollFunction();
-};
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () {
+// 	scrollFunction();
+// };
 
-function scrollFunction() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		mybutton.style.display = 'block';
-	} else {
-		mybutton.style.display = 'none';
-	}
-}
+// function scrollFunction() {
+// 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+// 		mybutton.style.display = 'block';
+// 	} else {
+// 		mybutton.style.display = 'none';
+// 	}
+// }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
