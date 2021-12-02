24a0213fbc4c6033c2a797f90ce58871cbbab4fe
solomonsanthosh <solosanty2064@gmail.com>
2021-12-02 10:37:09 +0530

    SS

--- public/javascripts/contactus.js
+++ public/javascripts/contactus.js
@@ -30,44 +30,43 @@ document.querySelector(".reset").addEventListener("click", (r) => {
 // 	}
 // });
 
-document.querySelector('.form').addEventListener('submit', (e) => {
-	var nameError = document.querySelector('.nameError');
-	var nameInput = document.querySelector('.name-input');
-	var phonenum = document.querySelector('.phoneError');
-	var phoneInput = document.querySelector('.phonenum');
-	var emailError = document.querySelector('.emailError');
-	var emailInput = document.querySelector('.email-input');
-	var commentError = document.querySelector('.commentError');
-	var commentInput = document.querySelector('.comment-input');
-	if (nameInput.value == '') {
-		nameError.style.display = 'block';
-		nameError.innerText = 'Please enter your name';
-		e.preventDefault();
-	}
-	if (phoneInput.value == '') {
-		phonenum.style.display = 'block';
-		phonenum.innerText = 'Please enter your number';
-		e.preventDefault();
-	}
-	if (emailInput.value == '') {
-		emailError.style.display = 'block';
-		emailError.innerText = 'Please enter your email';
-		e.preventDefault();
-	}
-	if (commentInput.value == '') {
-		commentError.style.display = 'block';
-		commentError.innerText = 'Please enter your comment';
-		e.preventDefault();
-	}
-  if(phoneInput.value.length != 10){
-  	phonenum.style.display = "block";
-  	phonenum.innerText = "Please enter 10 digit number";
-  	e.preventDefault();
+document.querySelector(".form").addEventListener("submit", (e) => {
+  var nameError = document.querySelector(".nameError");
+  var nameInput = document.querySelector(".name-input");
+  var phonenum = document.querySelector(".phoneError");
+  var phoneInput = document.querySelector(".phonenum");
+  var emailError = document.querySelector(".emailError");
+  var emailInput = document.querySelector(".email-input");
+  var commentError = document.querySelector(".commentError");
+  var commentInput = document.querySelector(".comment-input");
+  if (nameInput.value == "") {
+    nameError.style.display = "block";
+    nameError.innerText = "Please enter your name";
+    e.preventDefault();
+  }
+  if (phoneInput.value == "") {
+    phonenum.style.display = "block";
+    phonenum.innerText = "Please enter your number";
+    e.preventDefault();
+  }
+  if (emailInput.value == "") {
+    emailError.style.display = "block";
+    emailError.innerText = "Please enter your email";
+    e.preventDefault();
+  }
+  if (commentInput.value == "") {
+    commentError.style.display = "block";
+    commentError.innerText = "Please enter your comment";
+    e.preventDefault();
+  }
+
+  if (phoneInput.value.length != 10) {
+    phonenum.style.display = "block";
+    phonenum.innerText = "Please enter 10 digit number";
+    e.preventDefault();
   }
 });
 var inputs = document.querySelectorAll(".form-input");
-
-
 
 inputs.forEach((input) => {
   input.addEventListener("input", (e) => {
@@ -86,8 +85,6 @@ inputs.forEach((input) => {
       !commentInput.value == "" &&
       phoneInput.value.length == 10
     ) {
-      
-      
     } else {
       // submit.style.backgroundColor = "#AAAAAA";
 
@@ -98,8 +95,12 @@ inputs.forEach((input) => {
     }
   });
 });
-const  phoneInput = document.querySelector(".phonenum");
+const phoneInput = document.querySelector(".phonenum");
 phoneInput.addEventListener("input", (e) => {
+  var ASCIICode = e.target.value.slice(-1).charCodeAt(0);
+  if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57)) {
+    e.target.value = e.target.value.slice(0, -1);
+  }
   var phoneErr = document.querySelector(".phoneError");
   if (phoneInput.value.length !== 10) {
     e.preventDefault();
@@ -107,18 +108,19 @@ phoneInput.addEventListener("input", (e) => {
     phoneErr.innerText = "Please enter a 10 digit number";
   } else {
     phoneErr.style.display = "none";
-  } 
+  }
 });
+
 if (sweetp) {
-	function successdialog() {
-		Swal.fire({
-			position: 'center',
-			icon: 'success',
-			title: sweetp.innerText,
-			showConfirmButton: false,
-			timer: 2500
-		});
-	}
-	successdialog();
-	sweetp.parentNode.removeChild(sweetp);
+  function successdialog() {
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: sweetp.innerText,
+      showConfirmButton: false,
+      timer: 2500,
+    });
+  }
+  successdialog();
+  sweetp.parentNode.removeChild(sweetp);
 }
