ac48f2ff8b0240d18918f593ee8984c280db1910
Aravindhabii <abiiaravindhr.it2020@citchennai.net>
2022-01-21 16:22:09 +0530

    'Changes'

--- public/javascripts/results.js
+++ public/javascripts/results.js
@@ -76,6 +76,7 @@ dropdown.addEventListener('change', (e) => {
 					<span>
 						<h3>${item.name}</h3>
 						<p>${item.collegename}</p>
+						<h3>${item.mark!=0 ? item.mark+'/720':''}</h3>
 					</span>
 				</div>
 			`;
